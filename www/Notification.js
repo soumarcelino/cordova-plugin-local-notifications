472e139d8a1e538535ae398d4c536ca569173f49
soumarcelino <marcelinomatiasdesouza@gmail.com>
2019-12-27 15:18:32 -0300

    Update notification plugin methods

--- www/Notification.js
+++ www/Notification.js
@@ -1,29 +1,30 @@
 "use strict";
-function Launcher() {}
+function Notification() {}
 
-Launcher.prototype.FLAG_ACTIVITY_NEW_TASK = 0x10000000;
-
-Launcher.prototype.canLaunch = function (options, successCallback, errorCallback) {
-	options = options || {};
-	options.successCallback = options.successCallback || successCallback;
-	options.errorCallback = options.errorCallback || errorCallback;
-	cordova.exec(options.successCallback || null, options.errorCallback || null, "Launcher", "canLaunch", [options]);
+Notification.prototype.notification = function(
+  options,
+  successCallback,
+  errorCallback
+) {
+  options = options || {};
+  options.successCallback = options.successCallback || successCallback;
+  options.errorCallback = options.errorCallback || errorCallback;
+  cordova.exec(
+    options.successCallback || null,
+    options.errorCallback || null,
+    "Notification",
+    "notification",
+    [options]
+  );
 };
 
-Launcher.prototype.launch = function(options, successCallback, errorCallback) {
-	options = options || {};
-	options.successCallback = options.successCallback || successCallback;
-	options.errorCallback = options.errorCallback || errorCallback;
-	cordova.exec(options.successCallback || null, options.errorCallback || null, "Launcher", "launch", [options]);
+Notification.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.notification = new Notification();
+  return window.plugins.notification;
 };
 
-Launcher.install = function () {
-	if (!window.plugins) {
-		window.plugins = {};
-	}
-
-	window.plugins.launcher = new Launcher();
-	return window.plugins.launcher;
-};
-
-cordova.addConstructor(Launcher.install);
+cordova.addConstructor(Notification.install);
